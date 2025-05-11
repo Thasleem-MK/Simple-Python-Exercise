bc0feb8e867ea5799755403bfdbc9038aaca5087
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2025-05-11 14:09:21 +0530

    Timer

---
+++ TimerCountDown.py
@@ -0,0 +1,16 @@
+import time
+import sys
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = (x // 60) % 60
+    hours = x // 3600
+
+    print(f"\r{hours:02}:{minutes:02}:{seconds:02}", end="")
+    sys.stdout.flush()
+
+    time.sleep(1)
+
+print("\nTIME'S UP!")
